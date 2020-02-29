d9fc1366008f7bbd286cde4800c35c68d062c14f
Aaron Conway <aaronconwayphd@gmail.com>
2020-02-29 11:08:38 -0500

    trackHighlight

---
+++ R/trackHighlight.R
@@ -0,0 +1,20 @@
+#' Tracking highlighting
+#'
+#' Call this function as an addin to add the markdown track changes output for highlighting
+#'
+#' @export
+trackHighlight <- function() {
+  con <- rstudioapi::getSourceEditorContext()
+  
+  # Get selected text
+  selection <- con$selection[[1]]$text
+  
+  # Add markup
+  docPos <- con$selection[[1]]$range$end
+  rstudioapi::insertText(paste0("{==", selection, "==}{>><<}"),
+                         id = con$id)
+  
+  # Move cursor
+  docPosNew <- docPos + c(0, 9)
+  rstudioapi::setCursorPosition(docPosNew, id = con$id)
+}
